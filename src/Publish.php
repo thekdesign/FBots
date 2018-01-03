8f19fa7a507ce998d88ccb472840a64dcc5cfd97
ThekDesign <kevin830802@gmail.com>
2018-01-03 18:46:09 +0800

    :pencil2: fix chat method

--- src/Publish.php
+++ src/Publish.php
@@ -85,25 +85,37 @@ class Publish
         $reply
     ) {
 
-        $id = [];
+        $publish_id = [];
+
+        $comment_repeat = true;
 
         $publish_list = $this->get($page_id, 'comments');
 
         $publish_comments = json_decode($publish_list->getContent())->data;
 
-        $comment = $publish_comments[count($publish_comments) - 1];
+        $publish_last = $publish_comments[count($publish_comments) - 1];
+
+        $comment_replies = $this->get($publish_last->id, 'comments');
 
         foreach ($publish_comments as $publish_comment) {
 
-            $id[] = array_push($id, $publish_comment->from->id);
+            $publish_id[] = array_push($id, $publish_comment->from->id);
 
         }
 
-        if (array_unique($id) === $id) {
+        foreach ($comment_replies as $comment_reply) {
 
-            if ($hears === $comment->message) {
+            if ($comment_reply->from->id === substr($page_id, 0, 15)) {
+                $comment_repeat = false;
+            }
 
-                $this->reply($type_reply, $comment->id, $reply);
+        }
+
+        if (array_unique($publish_id) === $publish_id) {
+
+            if ($hears === $publish_last->message && $comment_repeat) {
+
+                $this->reply($type_reply, $publish_last->id, $reply);
 
             }
 
